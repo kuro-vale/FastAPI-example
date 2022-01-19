3e2938dc3135bf4477d87d86bdd3d5ba249675ee
kuro-vale <jsalcedo218@gmail.com>
2022-01-18 19:48:03 -0500

    Add user path operations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Python
 import json
+import os
 from typing import List
 import uuid
 # Models
@@ -8,35 +9,39 @@ from models.User import User
 from models.Tweet import Tweet
 # FastAPI
 from starlette.responses import RedirectResponse
-from fastapi import FastAPI, status, Body, HTTPException
-
+from fastapi import FastAPI, status, Body, HTTPException, Path
 app = FastAPI()
 
 
 # Path Operations
 @app.get("/", tags=["Home"])
 def home():
+    # Redirection to all tweets
     return RedirectResponse("/tweets", status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 
 
 # Auth Paths
-@app.post("/auth/signup", response_model=User, response_model_exclude={"password"}, status_code=status.HTTP_201_CREATED,
-          tags=["Auth"])
+@app.post("/auth/signup", status_code=status.HTTP_201_CREATED, tags=["Auth"])
 def sign_up(user: User = Body(...)):
+    # Add a User to users.json
     user.user_id = uuid.uuid4()
     with open("users.json", "r+", encoding="utf-8") as f:
         results = json.load(f)
+        for dictionary in results:
+            if user.username == dictionary["username"]:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exist")
         user_dict = user.dict()
         user_dict["user_id"] = str(user_dict["user_id"])
         user_dict["birth_date"] = str(user_dict["birth_date"])
         results.append(user_dict)
         f.seek(0)
         json.dump(results, f)
-        return user
+        return {user.username: "Created Successfully!"}
 
 
 @app.post("/auth/login", tags=["Auth"])
 def log_in(user: Login = Body(...)):
+    # If username and password match with one in users.json, then return a Login Successfully
     with open("users.json", "r", encoding="utf-8") as f:
         results = json.load(f)
         for dictionary in results:
@@ -46,27 +51,69 @@ def log_in(user: Login = Body(...)):
 
 
 # Users Paths
-@app.get("/users", response_model=List[User], response_model_exclude={"password"}, tags=["Users"])
+@app.get("/users", response_model=List[Login], response_model_exclude={"password"}, tags=["Users"])
 def show_all_users():
+    # Show all usernames in users.json
     with open("users.json", "r", encoding="utf-8") as f:
         results = json.load(f)
         return results
 
 
-@app.get("/users/{user_id}", response_model=User, response_model_exclude={"password"}, tags=["Users"])
-def show_user():
-    pass
+@app.get("/users/{username}", response_model=User, response_model_exclude={"password"}, tags=["Users"])
+def show_user(username: str = Path(..., max_length=20)):
+    # Show the info of a user in users.json
+    with open("users.json", "r", encoding="utf-8") as f:
+        results = json.load(f)
+        for dictionary in results:
+            if username == dictionary["username"]:
+                return dictionary
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This user doesn't exist")
 
 
-@app.delete("/users/{user_id}", response_model=User, response_model_exclude={"password"},
-            tags=["Users"],)  # status_code=status.HTTP_204_NO_CONTENT)
-def delete_user():
-    pass
+@app.delete("/users/{username}", tags=["Users"])
+def delete_user(username: str = Path(..., max_length=20)):
+    # Delete a user from users.json
+    with open("users.json", "r", encoding="utf-8") as f:
+        results = json.load(f)
+        for dictionary in results:
+            if username == dictionary["username"]:
+                results.remove(dictionary)
+                f.close()
+                with open("users.tmp", "w", encoding="utf-8") as t:
+                    json.dump(results, t)
+                    t.close()
+                    os.remove("users.json")
+                    os.rename("users.tmp", "users.json")
+                    return {username: "Deleted Successfully"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This user doesn't exist")
 
 
-@app.put("/users/{user_id}", response_model=User, response_model_exclude={"password"}, tags=["Users"])
-def update_user():
-    pass
+@app.put("/users/{username}", tags=["Users"])
+def update_user(username: str = Path(..., max_length=20), user: User = Body(...)):
+    # Update user from users.json
+    with open("users.json", "r+", encoding="utf-8") as f:
+        results = json.load(f)
+        repeated_username = 0
+        user_dict = user.dict()
+        for dictionary in results:  # Find username to be updated
+            if username == dictionary["username"]:
+                user_dict["user_id"] = str(dictionary["user_id"])  # Keep the same ID
+                user_dict["birth_date"] = str(user_dict["birth_date"])
+                dictionary.update(user_dict)
+        for dictionary in results:   # Prevent duplicates usernames
+            if user_dict["username"] == dictionary["username"]:
+                repeated_username += 1
+        if repeated_username == 1:  # If not duplicates, update the username in users.json
+            f.close()
+            with open("users.tmp", "w", encoding="utf-8") as t:
+                json.dump(results, t)
+                t.close()
+                os.remove("users.json")
+                os.rename("users.tmp", "users.json")
+                return {user_dict["username"]: "Updated Successfully"}
+        elif repeated_username > 1:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This user doesn't exist")
 
 
 # Tweets Paths
--- models/User.py
+++ models/User.py
@@ -1,11 +1,8 @@
-import json
 from datetime import date
-from os import path
 from typing import Optional
 from uuid import UUID
 
-from fastapi import HTTPException, status
-from pydantic import Field, EmailStr, validator
+from pydantic import Field, EmailStr
 
 from models.Login import Login
 
@@ -14,20 +11,6 @@ class User(Login):
     user_id: Optional[UUID] = Field(None)
     birth_date: Optional[date] = Field(None)
     email: EmailStr = Field(...)
-
-    @validator("username")
-    def not_exist(cls, v):
-        with open(path.dirname(__file__) + "/../users.json", "r", encoding="utf-8") as f:
-            results = json.load(f)
-            found = False
-            for dictionary in results:
-                if v == dictionary["username"]:
-                    found = True
-            if found:
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                    detail="Username already exists, please try another one")
-            else:
-                return v
 
     class Config:
         schema_extra = {
